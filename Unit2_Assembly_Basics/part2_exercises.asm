cd74f874dce21a5c5d212192fc102f91067a41b2
Linh Huynh <linhhuynh197200@gmail.com>
2024-02-01 16:39:47 +0000

    initial part2 commit

--- Unit2_Assembly_Basics/part2_exercises.asm
+++ Unit2_Assembly_Basics/part2_exercises.asm
@@ -8,14 +8,29 @@
 ;	- printing strings
 ;	- reading chars and ints
 
-%include "asm_io.inc"
+%include "asm_io.inc" ; directive
+
+age equ 30            ; symbol
+%define fav_number 34 ; macro
 
 ; initialized data
 segment .data
+fav_color db "purple", 0       ; string
+least_fav_color db "yellow", 0 ; string
+letter db "A"                  ; character
+number dd 95                   ; integer
+value dd 76                    ; integer (ascii L)
+hello db "Hello World!", 0     ; string    
+char_prompt db "Enter a character: ", 0
+number_prompt db "Enter a number: ", 0
+
+many_numbers times 5 dd 12 
+many_chars times 5 dd "Z"
 
 ; uninitialized data
 segment .bss
-
+space_for_number resd 1 ; space for 1 integer
+space_for_char resb 1 ; space for 1 character
 
 segment .text
         global  asm_main
@@ -23,7 +38,67 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+        ; mov dword[number], 116 
+	; mov eax, [number]
+        ; ; dump_regs 1
+
+        ; call print_int  ; print number in base 10
+        ; call print_nl
+
+        ; mov al, [letter]
+        ; call print_char 
+
+        ; EXERCISE: print ascii value and character of L
+	; mov eax, [value]
+        ; call print_int 
+        ; call print_nl
+        ; call print_char
+
+        ; dump_mem 1, fav_color, 2
+
+        ; mov eax, fav_color + 3 ; skip first 3 letters
+        ; call print_string 
+
+        ; EXERCISE: print hello world
+        ; mov eax, hello
+        ; call print_string
+        ; call print_nl
+        ; dump_mem 1, hello, 0
+
+        ; mov eax, char_prompt
+        ; call print_string
+        ; call read_char
+        ; call print_nl
+        ; call print_char
+
+        ; mov eax, [space_for_number]
+        ; call print_int
+        ; call print_nl
+
+        ; mov eax, number_prompt
+        ; call print_string
+        ; call read_int
+        ; mov dword[space_for_number], eax
+
+        ; mov eax, char_prompt
+        ; call print_string
+        ; call read_char
+        ; call print_nl
+        ; call print_char
+
+        ; mov eax, char_prompt
+        ; call print_string
+        ; call read_char
+        ; mov byte [space_for_char], al
+
+        ; mov eax, number_prompt
+        ; call print_string
+        ; call read_int
+        ; mov dword [space_for_number], eax
+
+        dump_mem 1, many_chars, 0
+        call print_nl
+        dump_mem 2, many_chars + 3, 0
 
         popa
         mov     eax, 0            ; return back to C
