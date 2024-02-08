bb799b909b017715bb73270d9708cb396c4d118d
Linh Huynh <linhhuynh197200@gmail.com>
2024-02-08 16:33:55 +0000

    initial shift/bitwise stuff

--- Unit3_Integers_And_Ops/shifts_exercises.asm
+++ Unit3_Integers_And_Ops/shifts_exercises.asm
@@ -1,5 +1,5 @@
 ; Base Author: Megan Avery Spring 2024
-; Exercise Author: [YOUR NAME HERE]
+; Exercise Author: Linh Huynh
 
 ; Purpose - to learn the following:
 ; 	- logical shifts
@@ -21,7 +21,20 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+        ; INCORRECT
+	; mov ebx, 3
+        ; mov eax, 20
+        ; shl eax, ebx
+        ; call print_int
+
+        ; mov eax, 23
+        ; shr eax, 2 ; divide by 4
+        ; call print_int
+
+        mov eax, 4
+        mov ebx, 2
+        test eax, ebx
+        dump_regs 1
 
         popa
         mov     eax, 0            ; return back to C
