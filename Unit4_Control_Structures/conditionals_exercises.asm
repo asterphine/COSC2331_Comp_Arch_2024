e76b5037dfdd9f81d91996e34e9b5862b2adbe0d
Linh Huynh <linhhuynh197200@gmail.com>
2024-02-29 16:33:18 +0000

    finally!

--- Unit4_Control_Structures/conditionals_exercises.asm
+++ Unit4_Control_Structures/conditionals_exercises.asm
@@ -1,5 +1,5 @@
 ; Base Author: Megan Avery Spring 2024
-; Exercise Author: [YOUR NAME HERE]
+; Exercise Author: Linh Huynh
 ;
 ; Purpose - to learn about conditionals in NASM
 
@@ -7,10 +7,14 @@
 
 
 segment .data					; initialized data
-
+if_prompt db "Enter a character: ", 0
+if_output db "and... your point is?", 0
+ifelse_prompt db "Enter a number: ", 0
+even db "EVEN", 0
+odd db "ODD", 0
 
 segment .bss					; uninitialized data
-
+character resb 1
 
 
 segment .text					; code
@@ -20,10 +24,33 @@ asm_main:
         pusha
 
 	; TODO: IF EXERCISE
+        mov eax, if_prompt
+        call print_string
+        call read_char
 
+        cmp eax, "&"            
+        jnz endif               ; if not an & jump to end
+        mov eax, if_output
+        call print_string
+endif:
+        call read_char          ; consume newline
+        call print_nl
 
 	; TODO: IF/ELSE EXERCISE
+        mov eax, ifelse_prompt
+        call print_string
+        call read_int
+        
+        and eax, 1              ; get last bit by itself
+        cmp eax, 0
+        je even_block
+        mov eax, odd
+        jmp end_ifelse
 
+even_block:
+        mov eax, even
+end_ifelse: 
+        call print_string
 
 	; TODO: ELIF EXERCISE
 
--- Unit4_Control_Structures/loops_exercises.asm
+++ Unit4_Control_Structures/loops_exercises.asm
@@ -1,11 +1,18 @@
 ; Base Author: Megan Avery Spring 2024
-; Exercise Author: [YOUR NAME HERE]
+; Exercise Author: Linh Huynh
 ;
 ; Purpose - to learn about loops in NASM
 
 %include "asm_io.inc"
 segment .data						; initialized data
+rocket db "🚀!", 0
 
+perfect_prompt db "Enter a power of 2: ", 0
+perfect db "PERFECT", 0
+another_perfect_prompt db "Again! ", 0
+
+fear_the_goat db "Fear the 🐐!", 0
+reaction db "Reaction? ", 0
 
 segment .bss						; uninitialized data
 
@@ -16,11 +23,55 @@ asm_main:
         enter   0,0               	; setup routine
         pusha
 
-		; TODO: FOR EXERCISE
+	; TODO: FOR EXERCISE
+;         mov ecx, 5
+; for:
+;         mov eax, ecx
+;         call print_int
+;         call print_nl
+;         loop for
 
-		; TODO: WHILE EXERCISE
+;         ; print rocket ship
+;         mov eax, rocket
+;         call print_string
 
-		; TODO: DO WHILE EXERCISE
+	; TODO: WHILE EXERCISE
+        mov eax, perfect_prompt
+        call print_string
+        call read_int
+while: 
+        mov ebx, eax
+        dec ebx
+        and eax, ebx
+        cmp eax, 0
+        jnz endwhile ; if missing infinite loop
+
+        ; when it is perfect power of 2
+        mov eax, perfect
+        call print_string
+        call print_nl
+
+        ; update - if missing while loop 
+        mov eax, another_perfect_prompt
+        call print_string
+        call read_int
+
+        jmp while
+endwhile:
+
+	; TODO: DO WHILE EXERCISE
+do:
+        call read_char
+        mov eax, fear_the_goat
+        call print_string
+        call print_nl
+
+        mov eax, reaction
+        call print_string
+        call read_char
+
+        cmp eax, "!"
+        jne do
 
         popa
         mov     eax, 0            ; return back to C
